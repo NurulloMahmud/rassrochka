a9bc5b346ba46f09a3850e7f2e498ec79ea116d6
NurulloMahmud <nurullomahmud@gmail.com>
2024-08-07 15:22:00 -0500

    items crud

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
-    UserRegisterAPIView, UserListAPIView
+    UserRegisterAPIView, UserListAPIView,
+    ItemCreateAPIView, ItemListAPIView, ItemRetrieveUpdateDestroyView
 )
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,6 +12,9 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('register/', UserRegisterAPIView.as_view()),
     path('users/', UserListAPIView.as_view()),
+    path('items/', ItemListAPIView.as_view()),
+    path('items/create/', ItemCreateAPIView.as_view()),
+    path('items/<int:pk>/', ItemRetrieveUpdateDestroyView.as_view()),
 
     # jwt
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
