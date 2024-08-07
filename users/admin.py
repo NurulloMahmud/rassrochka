b28783ca1544c4380038cc080c774d6607008471
NurulloMahmud <nurullomahmud@gmail.com>
2024-08-07 11:33:08 -0500

    login

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser, Item, Status
 
-# Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Item)
+admin.site.register(Status)
