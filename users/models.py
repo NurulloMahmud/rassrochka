cb83077a882ca869649b471cca6aac442eb46473
NurulloMahmud <nurullomahmud@gmail.com>
2024-08-08 09:16:47 -0500

    email null

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(validators=[phone_regex], unique=True, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     phone_number_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
