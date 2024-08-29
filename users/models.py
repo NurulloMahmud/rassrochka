e682d4cf6066ca8deeef1dc204c2048aec650736
NurulloMahmud <nurullomahmud@gmail.com>
2024-08-29 13:27:37 -0500

    created at

--- users/models.py
+++ users/models.py
@@ -41,6 +41,7 @@ class Item(models.Model):
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
