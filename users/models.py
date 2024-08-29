50a28c4f278761af785f4e96a9aff4639423066f
NurulloMahmud <nurullomahmud@gmail.com>
2024-08-29 13:14:39 -0500

    user field

--- users/models.py
+++ users/models.py
@@ -40,7 +40,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
